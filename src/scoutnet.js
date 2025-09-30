1a34002e7e388eb3e4678cf70b86916d8c8485bc
Petter Sandholdt <petter.sandholdt@sectra.com>
2025-09-30 15:58:03 +0200

    Remove unused getUserData function from ScoutNet API integration

--- src/scoutnet.js
+++ src/scoutnet.js
@@ -8,39 +8,6 @@ import * as storage from "./storage.js";
  */
 
 /**
- * Get User data from ScoutNet
- */
-export async function getUserData(member_id) {
-  const data = {};
-
-  const participant_data = await getParticipant(member_id);
-
-  if (participant_data) {
-    data["is_participant"] = participant_data["cancelled_date"] == null;
-    data["is_leader"] = participant_data["questions"]
-      ? participant_data["questions"]["82553"] === "55897"
-      : false;
-    data["is_ist"] = participant_data["questions"]
-      ? participant_data["questions"]["82555"] === "55897"
-      : false;
-    data["troop"] = participant_data["questions"]
-      ? participant_data["questions"]["82552"] || 0
-      : null;
-    data["patrol"] = participant_data["questions"]
-      ? participant_data["questions"]["82554"] || 0
-      : null;
-  } else {
-    data["is_participant"] = false;
-    data["is_leader"] = false;
-    data["is_ist"] = false;
-    data["troop"] = null;
-    data["patrol"] = null;
-  }
-
-  return data;
-}
-
-/**
  * Get specific participant data from ScoutNet
  * Return null if participant is missing
  */
